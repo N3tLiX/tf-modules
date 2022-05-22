da9275d8a89a987f79e8861cbc9089d6205c1f8c
Patrick Hayo <patrick.hayo@email.de>
2022-05-22 16:39:41 +0200

    updated

--- subnet/output.tf
+++ subnet/output.tf
@@ -5,8 +5,15 @@
 
 output "names" {
   description = "Contains a list of the resource name of the subnets"
-  value = [for subnet in azurerm_subnet.this : subnet.name
+  value = [for subnet in toset(azurerm_subnet.this) : subnet.name
     if subnet.name != "GatewaySubet" || subnet.name != "AzureFirewallSubnet" || subnet.name != "AzureFirewallManagementSubnet" || subnet.name != "AzureBastionSubnet"
+  ]
+}
+
+output "names_services" {
+  description = "Contains a list of the resource name of the subnets"
+  value = [for subnet in azurerm_subnet.this : subnet.name
+    if subnet.name == "GatewaySubet" || subnet.name == "AzureFirewallSubnet" || subnet.name == "AzureFirewallManagementSubnet" || subnet.name == "AzureBastionSubnet"
   ]
 }
 
